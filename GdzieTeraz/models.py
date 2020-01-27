ed09e6bb3eb10d6a2059d6a8ea5103e79a2957f1
Michał Miasojedow <michal@miasojedow.com>
2020-01-27 15:32:45 +0100

    tables model

--- GdzieTeraz/models.py
+++ GdzieTeraz/models.py
@@ -18,13 +18,25 @@ CITY = (
     (3, 'Wrocław'),
 )
 
+SIZES = (
+    (2, 2),
+    (4, 4),
+    (6, 6),
+    (8, 8),
+)
+
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=128, verbose_name='Nazwa')
     kitchen = models.IntegerField(choices=KITCHEN, verbose_name='Kuchania')
     city = models.IntegerField(choices=CITY, verbose_name='Miasto')
     address = models.CharField(max_length=128, verbose_name='Adres')
-    phone = models.IntegerField(max_length=9, verbose_name='Telefon')
+    phone = models.IntegerField(verbose_name='Telefon')
     www = models.URLField(verbose_name='Strona')
 
 
+class Tables(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64, verbose_name='Nazwa')
+    size = models.IntegerField(choices=SIZES, verbose_name='Wielkość')
+    taken = models.BooleanField(default=False, verbose_name='Zajęty')
